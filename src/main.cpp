eb49b648b5f00c04387df70c095ece73f9012605
Dennis Hardy <dennishardy@email.aarizona.edu>
2018-10-11 10:55:21 -0700

    Initial file input/output parsing.

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,42 @@ Valid data types include:
 ○ The number after Int and UInt specifies the width of the data input, output, register 
 *******************************************************************************/
 
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std; 
 
 int main(int argc, char *argv[]){
-   //std::cout << "Hello World!" << std::endl;
-   for(i = 0; i < argc; i++){
-      cout << "Argument " << i << " " << argv[i] << "\n";
+   //Alert user if invalid usage
+   if(argc != 3){
+      cout << "Input and/or output files not specified" << endl;
+      cout << "usage: dpgen netlistFile verilogFile" <<endl;
+      return -1;
    }
+
+   ifstream netlistFile;
+   ofstream verilogFile;
+   string line;
+   verilogFile.open(argv[2]);
+   netlistFile.open(argv[1]);
+
+   if(!verilogFile.is_open()){
+      cout << "Could not open " << argv[2] << endl;
+      return -1;
+   }
+   if(!netlistFile.is_open()){
+      cout << "Could not open " << argv[1] << endl;
+      return -1;
+   }
+   
+   while(!netlistFile.eof()){
+      if(getline(netlistFile, line)){
+         verilogFile << line << endl;
+      }
+   }
+
+   netlistFile.close();
+   verilogFile.close();
+   
    return 0;
 }
