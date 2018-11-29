d6947d1f85c065b83374f3adb375b23175802c1f
ziwang1995 <32801083+ziwang1995@users.noreply.github.com>
2018-11-28 21:58:23 -0700

    fix REG problem

--- src/io.cpp
+++ src/io.cpp
@@ -11,23 +11,23 @@
 
 using namespace std;
 
-vector<string> splitLine(string line){
+vector<string> splitLine(string line) {
 	vector<string> out;
 
 	stringstream stream;
 	stream.str(line);
-	
-	while(stream){
-      string temp;
+
+	while (stream) {
+		string temp;
 		stream >> temp;
-      if(temp.size()>0){
-         char last = temp[temp.length()-1];
-         if(last == ','){
-            temp = temp.substr(0, temp.size()-1);
-         }
-         out.push_back(temp);
-      }
+		if (temp.size()>0) {
+			char last = temp[temp.length() - 1];
+			if (last == ',') {
+				temp = temp.substr(0, temp.size() - 1);
+			}
+			out.push_back(temp);
+		}
 	}
-	
+
 	return out;
 }
